1f06db6c45c003204ebef2295c5d8ba040edee53
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-01-16 09:50:17 +0530

    all major components of homepage added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
 document.querySelector('video').playbackRate = 0.8;
+
+document.addEventListener('alpine:init', () => {
+   Alpine.store('accordion', {
+      tab: 0
+   });
+
+   Alpine.data('accordion', (idx) => ({
+      init() {
+         this.idx = idx;
+      },
+      idx: -1,
+      handleClick() {
+         this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
+      },
+      handleRotate() {
+         return this.$store.accordion.tab === this.idx ? 'rotate-180' : '';
+      },
+      handleToggle() {
+         return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
+      }
+   }));
+})
