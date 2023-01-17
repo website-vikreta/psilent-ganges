b92926a336ecf7ee13d1ee477efcbe291e3828fe
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2023-01-17 09:41:31 +0530

    final enhancement on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,32 @@ document.addEventListener('alpine:init', () => {
       }
    }));
 })
+
+
+var x = 0;
+$(window).scroll(function () {
+
+   var oTop = $('#stats-counter').offset().top - window.innerHeight + 200;
+   if (x == 0 && $(window).scrollTop() > oTop) {
+      $('.stats-counter-value').each(function () {
+         var $this = $(this),
+            countTo = $this.attr('data-count');
+         $({
+            countNum: $this.text()
+         }).animate({
+            countNum: countTo
+         },
+            {
+               duration: 2000,
+               easing: 'swing',
+               step: function () {
+                  $this.text(Math.floor(this.countNum));
+               },
+               complete: function () {
+                  $this.text(this.countNum);
+               }
+            });
+      });
+      x = 1;
+   }
+});
